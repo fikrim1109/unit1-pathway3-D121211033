3fe6927b1c5fc73e6209e032348d4fce32019179
fikrim1109 <88815201+fikrim1109@users.noreply.github.com>
2023-10-18 22:50:12 +0800

    push 1

---
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+//                  color = MaterialTheme.colorScheme.background
+                    color = Color(0xFF3ddc84)
+                ) {
+                    BusinessCardPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MiddlePart() {
+    val image = painterResource(id = R.drawable.android_logo)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier.background(Color(0xFF073042))
+        ) {
+            Image(painter = image, contentDescription = null, modifier = Modifier.size(160.dp))
+        }
+        Text(text = "Fikri Natadiwirya Maulana", fontSize = 30.sp, modifier = Modifier.padding(top = 16.dp))
+        Text(text = "D121211033", fontSize = 16.sp, fontWeight = FontWeight.SemiBold, color = Color(0xFF006833), modifier = Modifier.padding(top = 8.dp))
+    }
+}
+
+@Composable
+fun DataRow(text: String, icon: ImageVector, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .padding(top = 16.dp)
+    ) {
+        Icon(imageVector = icon, contentDescription = null, modifier = Modifier.size(25.dp), tint = Color(0xFF006833))
+        Text(text = text, fontSize = 20.sp, modifier = Modifier.padding(start = 10.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BusinessCardPreview() {
+    BusinessCardTheme{
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+        ) {
+
+            MiddlePart()
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .padding(top = 200.dp, bottom = 50.dp)
+            ) {
+                DataRow(text = "+62 851 564 85905", icon = Icons.Default.Call)
+                DataRow(text = "@FikriNatadiwiryaMaulana", icon = Icons.Default.Person)
+                DataRow(text = "Fikrimaulana1971@gmail.com", icon = Icons.Default.Email)
+            }
+        }
+    }
+}
